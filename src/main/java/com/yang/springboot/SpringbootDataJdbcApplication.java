54c8fdeca9145556552fc2e7ccb0c097fb89c26e
yangyue <gewei@yunbanhui.com>
2018-08-24 17:25:46 +0800

    SpringBoot练习--JDBC+Druid整合

---
+++ src/main/java/com/yang/springboot/SpringbootDataJdbcApplication.java
@@ -0,0 +1,12 @@
+package com.yang.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDataJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDataJdbcApplication.class, args);
+    }
+}
